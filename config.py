9c248458dfab4f24ce9e46ff6734111f20ef164a
Andrés Ricardo Rodríguez <AnriSyu>
2025-05-28 16:37:39 -0500

    ultimo

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-MYSQL_DATABASE = os.getenv('MYSQL_DATABASE', 'railway')
-MYSQL_HOST = os.getenv('MYSQLHOST', 'mysql.railway.internal')
+MYSQL_DATABASE = os.getenv('MYSQL_DATABASE', 'arduino_drive_trainer')
+MYSQL_HOST = os.getenv('MYSQLHOST', 'localhost')
 MYSQL_USER = os.getenv('MYSQLUSER', 'root')
-MYSQL_PASSWORD = os.getenv('MYSQLPASSWORD', 'UOuEJWSEEnxequxLBWBNeRrWHDpuShit')
+MYSQL_PASSWORD = os.getenv('MYSQLPASSWORD', '')
 MYSQL_PORT = int(os.getenv('MYSQLPORT', 3306))
